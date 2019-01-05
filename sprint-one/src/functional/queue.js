a349055d6edf3d757697e29470753f970684527b
Aimen <ymnghost@gmail.com>
2019-01-05 08:32:28 -0800

    Completed functional Stack and Queue functions

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,14 +5,37 @@ var Queue = function() {
   var storage = {};
 
   // Implement the methods below
-
+  var counter = 0;
+  
+  var firstInQueue = function(){
+    var smallest = counter;
+    for (var key in storage){
+      if (parseInt(key) < smallest){
+        smallest = parseInt(key);
+      }
+    }
+    return smallest;
+  };
+  
   someInstance.enqueue = function(value) {
+    storage[counter] = value;
+    counter++;
   };
 
   someInstance.dequeue = function() {
+    var keyBeingDequeued = firstInQueue();
+    var beingDequeued = storage[keyBeingDequeued];
+    delete storage[keyBeingDequeued];
+    console.log(beingDequeued);
+    return beingDequeued;
   };
 
   someInstance.size = function() {
+    var size = 0;
+    for (key in storage) {
+        if (storage.hasOwnProperty(key)) size++;
+    }
+    return size;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,18 +1,31 @@
 var Stack = function() {
-  var someInstance = {};
-
+  var someInstance = {}; 
   // Use an object with numeric keys to store values
   var storage = {};
+  var counter = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[counter] = value;
+    counter++;
   };
 
   someInstance.pop = function() {
+    counter--;
+    var bePopped = storage[counter.toString()];
+    delete storage[counter.toString()];
+    return bePopped;
+    
   };
 
   someInstance.size = function() {
+    var size = 0;
+    for (key in storage) {
+        if (storage.hasOwnProperty(key)) size++;
+    }
+    return size;
   };
+
 
   return someInstance;
 };
