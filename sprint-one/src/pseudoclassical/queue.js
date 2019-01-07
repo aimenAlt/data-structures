ebd806b252267a7b3d70bc9575e6358790eaa445
Aimen <ymnghost@gmail.com>
2019-01-07 00:59:34 -0800

    Completed all pseudoclassical functional classes

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,42 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  // but try not not reference your old code in writing the new style. 
+  this.queueStorage = {};
+  this.counter = 0;
 };
 
+Queue.prototype.firstInQueue = function(){
+  var smallest = this.counter;
+  console.log("firstInQueue");
+  for (var key in this.queueStorage){
+    
+    if (parseInt(key) < smallest) smallest = parseInt(key);
+  }
+  return smallest;
+};
 
+Queue.prototype.enqueue = function(value){
+//  console.log("enqueue");
+  this.queueStorage[this.counter] = value;
+  this.counter++;
+};
+
+Queue.prototype.dequeue = function(){
+//  console.log("dequeue");
+  var keyBeingdeQueued = this.firstInQueue();
+  var beingDequeued = this.queueStorage[keyBeingdeQueued];
+  delete this.queueStorage[keyBeingdeQueued];
+//  console.log();
+  return beingDequeued;
+};
+
+Queue.prototype.size = function(){
+  var length = 0;
+//  console.log("size");
+//  console.log(this.queueStorage);
+  for (var key in this.queueStorage){
+    length++;
+    console.log("size loop");
+  }
+  return length;
+};
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,26 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.counter = 0;
+  this.stackStorage = {};
+  
 };
 
+Stack.prototype.push = function(value) {
+  this.stackStorage[this.counter] = value
+  this.counter++;
+};
 
+Stack.prototype.pop = function() {
+  //console.log(`Pop Start ${this.counter}`);
+  this.counter--;
+  if (this.counter < 0) this.counter = 0;
+  var keyBeingPopped = this.counter;
+  var beingPopped = this.stackStorage[this.counter];
+  delete this.stackStorage[this.counter];
+  return beingPopped;
+};
+
+Stack.prototype.size = function() {
+  return this.counter;
+};
